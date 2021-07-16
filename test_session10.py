cb33bd4d48de1f79bf228bfa5688265fa13c6b5f
gops <50107085+vgops75@users.noreply.github.com>
2021-07-17 01:47:27 +0530

    Update test_session10.py
    Updated test_session10.py directly at github site, as there is some problem to git commit and push files from local directory.

--- test_session10.py
+++ test_session10.py
@@ -4,35 +4,43 @@ from sequencePolygon import SequencePolygon
 import pytest
 import random
 
-print(help(Polygon))
-print(len(Polygon.__doc__))
+# print(help(Polygon))
+# print(len(Polygon.__doc__))
 
-def test_docstrings_presence(cls):
-    if cls.__doc__ == None:
+def test_docstrings_Polygon():
+    if Polygon.__doc__ == None:
         return print(f'Documentation on class description missing')
     else:
-        if len(cls.__doc__) > 50:
+        if len(Polygon.__doc__) > 50:
             return print(f'class is described')
 
-test_docstrings_presence(Polygon)
-test_docstrings_presence(SequencePolygon)
+test_docstrings_Polygon()
 
-def test_compare_instances(cls):
-    p1 = cls(7, 5)
-    p2 = cls(3, 3)
+def test_docstrings_SequencePolygon():
+    if SequencePolygon.__doc__ == None:
+        return print(f'Documentation on class description missing')
+    else:
+        if len(SequencePolygon.__doc__) > 50:
+            return print(f'class is described')
+
+test_docstrings_SequencePolygon()
+
+def test_compare_instances():
+    p1 = Polygon(7, 5)
+    p2 = Polygon(3, 3)
     print(f'p1 is {p1}, and p2 is {p2}')
     print(f'greater_than inequality depends on vertices / num_edges alone')
     assert p1 > p2
 
-test_compare_instances(Polygon)
+test_compare_instances()
 
-def test_compare_type_instances(cls):
-    p1 = cls(7, 5)
+def test_compare_type_instances():
+    p1 = Polygon(7, 5)
     p2 = tuple((1, 2, 3))
     print(f'p1 is {p1}, and p2 is {type(p2)} {p2}')
     return p1 > p2
 
-test_compare_type_instances(Polygon)
+test_compare_type_instances()
 
 def test_SequencePolygon_is_sequence():
     import collections
